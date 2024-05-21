4954dae6296b57a9e3d8fa3a13e99a9349a482b2
N-Storm <N-Storm@users.noreply.github.com>
2024-05-21 12:47:13 +0300

    Pseudo-version v0.6.29

---
+++ software/cmake/i386-linux-toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+# set(CMAKE_SYSTEM_PROCESSOR x86)
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(CMAKE_EXE_LINKER_FLAGS "-static")
+
+# SET (TRIPLET arm-linux-gnueabihf)
+# SET(CMAKE_C_COMPILER ${TRIPLET}-gcc)
+
+# search for programs in the build host directories
+# SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+# SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
